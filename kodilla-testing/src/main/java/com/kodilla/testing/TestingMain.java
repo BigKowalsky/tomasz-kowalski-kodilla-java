4d4247341464ea39882bda32072c3f7e2828d831
Tomasz Kowalski <tomek.kowalski.89@gmail.com>
2022-06-21 22:44:58 +0200

    6.3 OddNumbersExterminator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,37 +1,6 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theFirstUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theFirstUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator myCalculator = new Calculator();
-
-        int a = 8;
-        int b = 6;
-
-        int sumResult = myCalculator.add(a, b);
-        if(sumResult == a + b) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        int differenceResult = myCalculator.substract(a,b);
-        if(differenceResult == a - b) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-        }
+    }
     }
